638b791fc51f91ffc4becd6fd0e00a4fdc0cb70b
Juhee Noh <nohjuhee227@gmail.com>
2023-03-07 17:15:54 +0900

    [learn] 17.6 리덕스 더 편하게 사용하기

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import Counter from '../components/Counter';
 import { decrease, increase } from '../modules/counter';
 
